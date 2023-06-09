18cbf75333e83041a1158c09979eed66806d26e2
inkwje <dng.lorenzo.00@proton.me>
2023-06-09 12:56:33 +0200

    fix: s/{device-num}/{device-name}/gm
    yeah, i love regex

--- waybar/whq/audio-switcher.sh
+++ waybar/whq/audio-switcher.sh
@@ -4,7 +4,7 @@ get_card=$(pactl get-default-sink)
 
 if [[ $get_card =~ "BEHRINGER_UMC404HD" ]]
     then
-    pactl set-default-sink 1014
+    pactl set-default-sink alsa_output.usb-Dell_Dell_AC511_USB_SoundBar-00.analog-stereo
     else
-    pactl set-default-sink 47 
+    pactl set-default-sink alsa_output.usb-BEHRINGER_UMC404HD_192k-00.analog-surround-40
 fi
